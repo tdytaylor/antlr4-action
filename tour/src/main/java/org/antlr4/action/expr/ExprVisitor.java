a362348bb0c4bfbb16d62417995a8b457354d984
tdytaylor <949722633@qq.com>
2021-05-30 21:31:15 +0800

    feat: init

---
+++ tour/src/main/java/org/antlr4/action/expr/ExprVisitor.java
@@ -0,0 +1,30 @@
+package org.antlr4.action.expr;// Generated from /Users/tdytaylor/github/antlr4-action/tour/src/antlr4.expr/Expr.g4 by ANTLR 4.9.1
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ExprParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ExprVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#prog}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProg(ExprParser.ProgContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStat(ExprParser.StatContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpr(ExprParser.ExprContext ctx);
+}
